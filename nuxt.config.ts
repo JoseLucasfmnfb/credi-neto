c86f3b606497385d43078f9048cffd18b23e1319
JoseOliveira <joseoliveira@overdriveconsultoria.com.br>
2026-01-23 19:44:24 -0300

    chore: init nuxt minimal project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,5 @@
-import { config } from 'dotenv'
-config({ path: '.env' })   // 👈 força carregar o arquivo .env
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-
-  hooks: {
-    'ready'() {
-      console.log('TESTE SUPABASE_URL:', process.env.SUPABASE_URL)
-      console.log('TESTE SUPABASE_KEY:', process.env.SUPABASE_KEY)
-    }
-  },
-
-  modules: [
-    '@nuxtjs/supabase',
-    '@nuxtjs/tailwindcss'
-  ],
-
-  supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
-    redirect: false,
-    redirectOptions: {
-      login: '/',
-      callback: '/confirm',
-      exclude: ['/register']
-    }
-  }
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true }
 })
